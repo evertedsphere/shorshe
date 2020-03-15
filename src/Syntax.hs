98cf23977db7e103ac705b87d517e0ce8f33e9d7
Soham Chowdhury <chow.soham@gmail.com>
2020-03-16 00:34:43 +0530

    some more path/place functions, 2

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,19 +1,26 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Syntax where
 
-import Control.Lens ((<&>))
+import Control.Lens ((&), (<&>))
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HashSet
+import Data.Hashable
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
+import GHC.Exts (IsList)
 
 type Vec =
   Vector
@@ -22,12 +29,12 @@ type Vec =
 -- TODO(mrkgnao): move to a byte offset-based representation
 data SrcPos
   = SrcPos (Int, Int)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 -- | A source location
 data SrcLoc
   = SrcLoc Text SrcPos SrcPos
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 invalid :: SrcPos
 invalid =
@@ -47,38 +54,39 @@ dummy =
 
 newtype Var
   = MkVar Text
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Vars =
   Vec Var
 
 newtype TyVar
   = MkTyVar Text
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type TyVars =
   Vec TyVar
 
 newtype FnVar
   = MkFnVar Text
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 newtype ProvVar
   = MkProvVar Text
-  deriving stock (Show)
+  deriving stock (Show, Eq)
+  deriving newtype (Hashable)
 
 newtype StructVar
   = MkStructVar Text
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data Owned
   = Shared
   | Unique
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data EnvVar
   = MkEnvVar Owned Text
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type EnvVars =
   Vec EnvVar
@@ -88,7 +96,7 @@ type EnvVars =
 data SubtypeModality
   = Combine
   | Override
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Field =
   Text
@@ -99,19 +107,19 @@ type TypedField =
 data PathEntry
   = Field Field
   | Index Int
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 newtype Path
   = Path (Vec PathEntry)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data PrePlace
   = PrePlace Var Path
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data Place
   = Place SrcLoc PrePlace
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Places =
   Vec Place
@@ -122,20 +130,20 @@ data ExpPathEntry
   = ExpField Field
   | ExpIndex Int
   | ExpDeref
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 newtype ExpPath
   = ExpPath (Vec ExpPathEntry)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data PrePlaceExp
   = PrePlaceExp Var ExpPath
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
--- | A place expression.
+-- | A place expession.
 data PlaceExp
   = PlaceExp SrcLoc PrePlaceExp
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type PlaceExps =
   Vec PlaceExp
@@ -146,11 +154,11 @@ rootOf (Place _ (PrePlace v _)) = v
 pathOf :: Place -> Path
 pathOf (Place _ (PrePlace _ p)) = p
 
-exprRootOf :: PlaceExp -> Var
-exprRootOf (PlaceExp _ (PrePlaceExp v _)) = v
+expRootOf :: PlaceExp -> Var
+expRootOf (PlaceExp _ (PrePlaceExp v _)) = v
 
-exprPathOf :: PlaceExp -> ExpPath
-exprPathOf (PlaceExp _ (PrePlaceExp _ p)) = p
+expPathOf :: PlaceExp -> ExpPath
+expPathOf (PlaceExp _ (PrePlaceExp _ p)) = p
 
 toExpPathEntry :: PathEntry -> ExpPathEntry
 toExpPathEntry = \case
@@ -164,7 +172,7 @@ toPlaceExp :: Place -> PlaceExp
 toPlaceExp (Place loc (PrePlace root path)) =
   PlaceExp loc (PrePlaceExp root (toExpPath path))
 
--- | Convers expression path entries to the corresponding path entry forms.
+-- | Convers expession path entries to the corresponding path entry forms.
 toPathEntry :: ExpPathEntry -> Maybe PathEntry
 toPathEntry = \case
   ExpField f -> Just (Field f)
@@ -179,30 +187,50 @@ toPath (ExpPath ep) = foldr go (Just (Path [])) ep
       entry <- toPathEntry expEntry
       pure (Path (Vec.cons entry path))
 
+isPlace :: PlaceExp -> Bool
+isPlace pexp = not (Vec.elem ExpDeref entrys)
+  where
+    ExpPath entrys = expPathOf pexp
+
 data Loan
   = Loan Owned PlaceExp
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Loans =
   Vec Loan
 
 data Prov
-  = Prov SrcLoc ProvVar
-  deriving stock (Show)
+  = Prov {_provSrcLoc :: SrcLoc, _provProvVar :: ProvVar}
+  deriving stock (Show, Eq)
 
-type Provs =
-  Vec Prov
+newtype Provs
+  = Provs (Vec Prov)
+  deriving stock (Show, Eq)
+  deriving newtype (IsList)
+
+containsProv :: Prov -> Provs -> Bool
+containsProv (Prov _ v) (Provs provs) =
+  provs & Vec.map _provProvVar & Vec.elem v
 
 data Bound
   = Bound Prov Prov
+  deriving stock (Show, Eq)
+
+upperProvVar :: Bound -> ProvVar
+upperProvVar (Bound _ (Prov _ v)) = v
+
+newtype Bounds
+  = Bounds (Vec Bound)
   deriving stock (Show)
 
-type Bounds =
-  Vec Bound
+instance Eq Bounds where
+  Bounds bs == Bounds bs' = upperProvVars bs == upperProvVars bs'
+    where
+      upperProvVars = HashSet.fromList . Vec.toList . Vec.map upperProvVar
 
 data Param
-  = Param Var Ty
-  deriving stock (Show)
+  = Param {_paramVar :: Var, _paramTy :: Ty}
+  deriving stock (Show, Eq)
 
 type Params =
   Vec Param
@@ -211,7 +239,7 @@ data BaseTy
   = Bool
   | U32
   | Unit
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data PreTy
   = TyAny
@@ -226,25 +254,25 @@ data PreTy
   | TyTup Tys
   | TyStruct StructVar Provs Tys (Maybe Ty)
   | TyUninit Ty
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data Ty
   = Ty SrcLoc PreTy
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data Env
   = EnvUnboxed
   | EnvVar EnvVar
   | Env VarEnv
   | EnvOf Var
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Envs =
   Vec Env
 
 newtype VarEnv
   = VarEnv (Vec Param)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Gamma =
   VarEnv
@@ -262,18 +290,54 @@ data PreTyCtx
   | CtxTagged StructVar Provs Tys TyCtx
   | CtxRec (Vec (Field, TyCtx))
   | CtxTup (Vec TyCtx)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
+-- | Invariant: a type-context should only ever have one hole.
 data TyCtx
   = TyCtx SrcLoc PreTyCtx
-  deriving stock (Show)
+  deriving stock (Show, Eq)
+
+isSized :: Ty -> Bool
+isSized (Ty _ ty) = case ty of
+  TySlice _ -> False
+  _ -> True
+
+class InitCk a where
+  isInit :: a -> Bool
+
+instance InitCk Ty where
+  isInit (Ty _ typ) = case typ of
+    TyAny -> True
+    TyInfer -> True
+    TyBase {} -> True
+    TyVar {} -> True
+    -- invariant: this should always be true
+    TyRef _ _ ty -> isInit ty
+    TyFun _ _ _ tys gamma ty _ ->
+      isInit tys && isInit gamma && isInit ty
+    TyArr ty _ -> isInit ty
+    TySlice ty -> isInit ty
+    TyRec flds -> isInit (Vec.map snd flds)
+    TyStruct _ _ tys (Just ty) -> isInit tys && isInit ty
+    TyStruct _ _ tys Nothing -> isInit tys
+    TyUninit {} -> False
+
+instance InitCk Tys where
+  isInit = all isInit
+
+instance InitCk Env where
+  isInit = \case
+    EnvUnboxed -> True
+    EnvVar {} -> True
+    Env (VarEnv params) -> isInit (Vec.map _paramTy params)
+    EnvOf {} -> True
 
 data Prim
   = PrimTrue
   | PrimFalse
   | PrimUnit
   | PrimNum Int
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data Binop
   = -- | addition
@@ -312,7 +376,7 @@ data Binop
     Ge
   | -- | greater than
     Gt
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data PreExp
   = ExpPrim Prim
@@ -338,11 +402,11 @@ data PreExp
   | ExpRecStruct StructVar Provs Tys (Vec (Field, Exp))
   | ExpTupStruct StructVar Provs Tys Exps
   | ExpPtr Owned Place
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data Exp
   = Exp SrcLoc PreExp
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Exps =
   Vec Exp
@@ -354,11 +418,11 @@ data Value
   | ValTup (Vec Value)
   | ValArr (Vec Value)
   | ValPtr Owned Place
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 newtype Store
   = Store (Vec (Var, Value))
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data FnDef
   = FnDef
@@ -372,30 +436,32 @@ data FnDef
         _fnDefBounds :: Bounds,
         _fnDefBody :: Exp
       }
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
+-- TODO(mrkgnao): convert to record, what is the bool?
 data RecStructDef
   = RecStructDef Bool StructVar Provs TyVars (Vec TypedField)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
+-- TODO(mrkgnao): convert to record, what is the bool?
 data TupStructDef
   = TupStructDef Bool StructVar Provs TyVars Tys
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data StructDef
   = StructRec RecStructDef
   | StructTup TupStructDef
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data GlobalDef
   = DefFn FnDef
   | DefRecStruct RecStructDef
   | DefTupStruct TupStructDef
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 newtype GlobalEnv
   = GlobalEnv (Vec GlobalDef)
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Sigma =
   GlobalEnv
@@ -404,13 +470,26 @@ emptySigma :: GlobalEnv
 emptySigma =
   GlobalEnv []
 
-data SubTy
-  = SubTy ProvVar ProvVar
-  deriving stock (Show)
+findFnDef :: GlobalEnv -> FnVar -> Maybe FnDef
+findFnDef (GlobalEnv sigma) fn = do
+  DefFn fnDef <- Vec.find isRightFn sigma
+  pure fnDef
+  where
+    isRightFn :: GlobalDef -> Bool
+    isRightFn = \case
+      DefFn fnDef -> fn == _fnDefName fnDef
+      DefTupStruct (TupStructDef _ (MkStructVar v) _ _ _) -> fn == MkFnVar v
+      DefRecStruct {} -> False
+
+-- TODO continue from here
+
+data SubtypeRel
+  = SubtypeRel ProvVar ProvVar
+  deriving stock (Show, Eq)
 
 data TyVarEnv
-  = TyVarEnv EnvVars Provs TyVars (Vec SubTy)
-  deriving stock (Show)
+  = TyVarEnv EnvVars Provs TyVars (Vec SubtypeRel)
+  deriving stock (Show, Eq)
 
 type Delta =
   TyVarEnv
@@ -421,7 +500,7 @@ emptyDelta =
 
 newtype LoanEnv
   = LoanEnv (Vec (Prov, Loans))
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 type Ell =
   LoanEnv
@@ -432,12 +511,12 @@ emptyEll =
 
 newtype PlaceEnv
   = PlaceEnv (Vec (Place, Ty))
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data StructKind
   = Rec
   | Tup
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data TcErr
   = -- | expected, found
@@ -454,7 +533,7 @@ data TcErr
   | NoReferenceToParameter Place
   | NoReferenceToCaptured Place
   | CannotMove PlaceExp
-  | -- | expr in function position, the uninitialized type
+  | -- | exp in function position, the uninitialized type
     MovedFunction Exp Ty
   | -- | place that was moved the type for it
     PartiallyMoved Place Ty
@@ -492,7 +571,7 @@ data TcErr
   | -- | unbound loan in Prov
     UnboundLoanInProv PlaceExp Prov
   | PlaceExpNotAPlace PlaceExp
-  | AbsProvsNotSubType Prov Prov
+  | AbsProvsNotSubtype Prov Prov
   | -- | cannot promote local Prov to abstract Prov
     CannotPromoteLocalProvToAbstract Prov Prov
   | EnvArityMismatch Text Envs EnvVars
@@ -500,4 +579,4 @@ data TcErr
   | TysArityMismatch Text Tys Tys
   | TyArityMismatch Text Tys TyVars
   | ExpArityMismatch Text Exps Tys
-  deriving stock (Show)
+  deriving stock (Show, Eq)
